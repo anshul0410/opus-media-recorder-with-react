dd4ef15b24bb2e9c98f069b8d28e2f828e4c9613
anshul0410 <anshul041094@gmail.com>
2020-08-25 16:10:33 +0530

    [update] initialize git repo

---
+++ README.md
@@ -0,0 +1,1 @@
+Project to implement opus-media-recorder with react js
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import OpusMediaRecorderView from './OpusMediaRecorderView'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const data = [];
+    this.state = { data: data, blob: new Blob(data)};
+  }
+
+  render() {
+    console.log(this.state.blob,'this.state.blob')
+    return (
+      <div className="App">
+        <OpusMediaRecorderView
+          onDataAvailable={(e) => {
+            const data = [...this.state.data, e.data];
+            this.setState({
+              data: data,
+              blob: new Blob(data)
+            })
+          }}
+          render={({ state, start, stop, pause, resume }) => (
+            <div>
+              <p>{state}</p>
+              <button onClick={start}>Start Recording</button>
+              <button onClick={stop}>Stop Recording</button>
+              <audio
+                src={this.state.data.length ? URL.createObjectURL(this.state.blob) : ''}
+                controls
+              />
+            </div>
+          )}
+        />
+      </div>
+    )
+  }
+}
+
+export default App;
