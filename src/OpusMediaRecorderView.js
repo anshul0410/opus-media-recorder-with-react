a2aa97e0fe62f2848f7238274fa802388efc5ea5
anshul0410 <anshul041094@gmail.com>
2020-08-25 16:19:05 +0530

    [update] installed hark for user speech input

--- src/OpusMediaRecorderView.js
+++ src/OpusMediaRecorderView.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import MediaRecorder from 'opus-media-recorder';
+import hark from 'hark';
 
 // opus-media-recorder options
 const workerOptions = {
